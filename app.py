5a9a556580e9cf7ae71070f4a80eba128e423430
rvgoing <97498926+rvgoing@users.noreply.github.com>
2025-02-19 23:54:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import os
 app = Flask(__name__)
 
 # Load credentials from environment variables
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("Vx9EqfAePjUC2/AAllYO0LhE3m5l/VkUTfGDlxMvuIC4Mf635WTB8G+Y1aikUj8FLRbdFvEkFRdwR5dYRSvX5gqB64fu3I9akViE7GJmlfoK0+vjbpgReNo0TPAkhGOqr7rFHb91QpkUaeFoQvMz2AdB04t89/1O/w1cDnyilFU=")
-LINE_CHANNEL_SECRET = os.getenv("6262aa5eb114fbd6916ed6fa7e78d18b")
+# LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+# LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+LINE_CHANNEL_ACCESS_TOKEN = "Vx9EqfAePjUC2/AAllYO0LhE3m5l/VkUTfGDlxMvuIC4Mf635WTB8G+Y1aikUj8FLRbdFvEkFRdwR5dYRSvX5gqB64fu3I9akViE7GJmlfoK0+vjbpgReNo0TPAkhGOqr7rFHb91QpkUaeFoQvMz2AdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "6262aa5eb114fbd6916ed6fa7e78d18b"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
